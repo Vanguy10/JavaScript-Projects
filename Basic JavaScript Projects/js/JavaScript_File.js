fb381b69d26ba4c4f8a0cf639588c6181b2bea21
Joey <vandude10@yahoo.com>
2020-07-21 10:41:07 -0700

    step_64

--- Basic JavaScript Projects/js/JavaScript_File.js
+++ Basic JavaScript Projects/js/JavaScript_File.js
@@ -1,5 +1,11 @@
-function My_firstfunction() {   //Definig a function and naaming it
+function My_First_Function() {   //Definig a function and naaming it
     var str="This text is green!";//defining a variable and giving it a string value
     var result=str.fontcolor("green"); //using the fontcolor method on str variable
     document.getElementById("Green_Text").innerHTML=result; //putting the value of result int HTML element with "Green_Text" id
+}
+
+function myFunction () {
+    var sentence="I am learning";
+    sentence += "a lot from this book!";
+    document.getElementById("Concatenate").innerHTML=sentence;
 }
