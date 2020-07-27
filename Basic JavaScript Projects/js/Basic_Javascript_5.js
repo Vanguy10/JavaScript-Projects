1f72f8643ab50ba6574979592cb23c4a725a3c09
Joey <vandude10@yahoo.com>
2020-07-26 19:21:23 -0700

    step_116

--- Basic JavaScript Projects/js/Basic_Javascript_5.js
+++ Basic JavaScript Projects/js/Basic_Javascript_5.js
@@ -1,7 +1,53 @@
 function my_Function() {
-document.getElementById("Test").innerHTML=0/0;
-}
+    document.getElementById("Test").innerHTML=0/0;
+    }
+
+function not_Function() {
+    document.getElementById("Not").innerHTML= !(5>10);
+    }
+
 
 document.write(2E310);
+document.write("<br>");
 
+document.write("<br>");
 document.write(10>2);
+document.write("<br>");
+
+document.write("<br>");
+console.log(2+2);
+document.write("<br>");
+
+document.write("<br>");
+document.write("10"+5);
+document.write("<br>");
+
+document.write("<br>");
+console.log(4>6);
+document.write("<br>");
+
+document.write("<br>");
+document.write(10==10);
+document.write("<br>");
+
+document.write("<br>");
+document.write(10==12);
+document.write("<br>");
+
+document.write("<br>");
+x=10;
+y=10;
+document.write(x===y);
+document.write("<br>");
+
+document.write("<br>");
+x=82;
+y="82";
+document.write(x===y)
+document.write("<br>");
+
+document.write("<br>");
+document.write(5>2 && 10>4);
+document.write(5>10 && 10>4);
+document.write(5>10 || 10>4);
+document.write(5>10 || 10>20);
