4837f848dfc348c29a6e94b0d7a0384957efd433
Joey <vandude10@yahoo.com>
2020-08-14 00:12:35 -0700

    step_157

--- Basic JavaScript Projects/js/Basic_Javascript_8.js
+++ Basic JavaScript Projects/js/Basic_Javascript_8.js
@@ -46,3 +46,17 @@ function myFunction() {
     var res=str.valueOf();
     document.getElementById("valueof").innerHTML=res;
 }
+
+function countdown() {
+    var seconds=document.getElementById("seconds").value;
+
+    function tick() {
+        seconds=seconds -1;
+        timer.innerHTML=seconds;
+        setTimeout(tick,1000);
+    if(seconds == 1){
+        alert("Time's up!");
+    }
+    }
+    tick();
+}
